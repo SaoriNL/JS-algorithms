8a1f592eb376c27f08aafe347ce54f80f0d0e0b1
Saori Mita <saorimita4@gmail.com>
2022-05-02 10:02:56 +0200

    selectionsort

--- selectionsort.js
+++ selectionsort.js
@@ -1,11 +1,29 @@
-function selectionsort(array)
-  min = array[i]
-  while(i = 0; i > array.length; i++)
-    if (min < rest of array){
-      array[0] = temp;
-      temp = min;
-    } else if (min > some smaller array){
-      array[0] = temp;
-      temp = array[0];
+function selectionSort(arr){
+  for(var i = 0; i < arr.length; i++){
+    var lowest = i;
+    for(var j = i+1; j < arr.length; j++){
+      if(arr[j] < arr[lowest]){
+        lowest = j;
+      }
     }
-    return array;
+    if(i !== lowest){
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+
+// function selectionsort(array)
+//   let min = array[i]
+//   while(i = 0; i > array.length; i++)
+//     if (min < rest of array){
+//       let temp = array[0];
+//       temp = min;
+//     } else if (min > some smaller array){
+//       array[0] = temp;
+//       temp = array[0];
+//     }
+//     return array;
