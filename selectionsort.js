ed5a9d5047eb1205a3fe46992e48a2d15e93f8c1
Saori Mita <saorimita4@gmail.com>
2022-05-02 09:37:24 +0200

    pseudocode

---
+++ selectionsort.js
@@ -0,0 +1,10 @@
+function selectionsort(array)
+  min = array[i]
+  while(i = 0; i > array.length; i++)
+    if (min < rest of array){
+      array[0] = temp;
+      temp = min;
+    } else if (min > some smaller array){
+      array[0] = temp;
+      temp = array[0];
+    }
